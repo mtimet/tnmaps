1d01d8a26c5c9d342ac6546dc25c41dff06dedc6
M'timet Mohamed Ali <mohamedali.mtimet@gmail.com>
2012-02-06 21:31:17 +0000

    working example with bare minimum

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-var xym = d3.geo.mercator();
 
-var translate = xym.translate();
-translate[0] = -17;
-translate[1] = 2000;
+var proj = d3.geo.mercator().scale(1).translate([0, 0]),
+    path = d3.geo.path().projection(proj);
 
-xym.translate(translate);
-xym.scale(17838);
+var margin = 30,
+    width = window.innerWidth - margin,
+    height = window.innerHeight - margin;
 
-var svg = d3.select("body").append("svg");
-svg.append("g").attr("id", "states");
+var stl = "fill: rgb(209, 210, 213); fill-opacity: 1; stroke-width: 1.87696; stroke-miterlimit: 4; stroke-dasharray: none; stroke: rgb(255, 255, 255); stroke-opacity: 1;";
 
-d3.json("geojson/delegations.geojson", function(collection) {
-  var map = svg.select("#states")
-               .selectAll("path")
-               .data(collection.features)
-               .enter()
-               .append("path")
-               .attr("d", d3.geo.path().projection(xym));
+var svg = d3.select("body")
+        .append("svg:svg")
+        .attr("height", height)
+        .attr("width", width);
+var map = svg.append("svg:g");
+
+d3.json("geojson/delegations.geojson", function(json) {
+    var bounds0 = d3.geo.bounds(json),
+        bounds = bounds0.map(proj),
+        xscale = width/Math.abs(bounds[1][0] - bounds[0][0]),
+        yscale = height/Math.abs(bounds[1][1] - bounds[0][1]),
+        scale = Math.min(xscale, yscale);
+
+    proj.scale(scale);
+    proj.translate(proj([-bounds0[0][0], -bounds0[1][1]]));
+
+    map.selectAll("path")
+        .data(json.features)
+        .enter().append("svg:path")
+        .attr("d", path)
+        .attr("style", stl);
 });
