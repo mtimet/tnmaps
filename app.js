56953a6c99352a38797dc5603e7494fbfe691014
M'timet Mohamed Ali <mohamedali.mtimet@gmail.com>
2012-02-11 03:54:30 +0100

    labels, colour switching, circonscription info

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
 
 
 
-var width = $("#map").width();
-var height = width;
+var width = $("#map-delegations").width();
+var height = width * 3 / 4;
 
-var svg = d3.select("#map")
+var delegationsSVG = d3.select("#map-delegations")
     .append("svg:svg")
-    .attr("class", "hero-unit")
+    .attr("class", "RdPu")
     .attr("height", height)
     .attr("width", width);
 
-var map = svg.append("svg:g");
-window.delegations = null;
+var delegationsMap = delegationsSVG.append("svg:g");
+var _selectedCirconscription = $("#select-circonscription").val();
+var _addLabels = $("#add-labels").is(":checked");
+var _selectedDelegation = null;
+var _selectedFeature = null;
+var _data = null;
 
-load_delegation("tunis1");
-
-d3.select("#select-delegation").on("change", function() {
-  load_delegation(this.value);
+d3.select("#select-circonscription").on("change", function() {
+    _selectedCirconscription = this.value;
+    update(delegationsMap, width, height);
 });
 
-function load_delegation(name) {
-d3.json("geojson/" + name + ".geojson", function(json) {
+d3.select("#select-color").on("change", function() {
+    d3.selectAll("svg").attr("class", this.value);
+});
 
-var proj = d3.geo.mercator().scale(1).translate([0, 0]),
+d3.select("#add-labels").on("change", function() {
+    _addLabels = this.checked;
+    update(delegationsMap, width, height);
+});
+
+update(delegationsMap, width, height);
+
+function update(svg, width, height) {
+    d3.json("geojson/" + _selectedCirconscription + ".geojson", function(json) {
+        _data = json;
+	var path = getProjectionPath(json, width, height);
+	updateMap(svg, json, path);
+	updateLabels(svg, json, path);
+	updateInfoBox();
+    })
+};
+
+
+function getProjectionPath(json, width, height){
+    var proj = d3.geo.mercator().scale(1).translate([0, 0]),
     path = d3.geo.path().projection(proj);
 
     var bounds0 = d3.geo.bounds(json),
@@ -33,17 +56,21 @@ var proj = d3.geo.mercator().scale(1).translate([0, 0]),
 
     proj.scale(scale);
     proj.translate(proj([-bounds0[0][0], -bounds0[1][1]]));
-    
-    delegations = map.selectAll("path")
-        .data(json.features, function (d) {
-            return d.properties.name_1 + " " + d.properties.name_2;
-        });
 
-    delegations
+    return path;
+}
+ 
+function updateMap(svg, json, path) {
+    var features = svg.selectAll("path")
+        .data(json.features, function (d) {
+	    return d.properties.name_1 + " " + d.properties.name_2;
+        });
+    
+    features
         .enter()
         .append("svg:path");
-
-    delegations
+    
+    features
         .attr("class", quantize)
         .attr("id", function(d) {return d.properties.code_deleg ? d.properties.code_deleg : null})
         .attr("d", path)
@@ -56,43 +83,114 @@ var proj = d3.geo.mercator().scale(1).translate([0, 0]),
         .on("click", click)
         .on("mouseover", mouseover)
         .on("mouseout", mouseout);
-
-    delegations
+    
+    features
         .append("svg:title")
-        .text(function(d) {return d.properties.name_deleg});
-        
-    delegations.exit().remove(); 
-
-    function quantize(d) {
-	return d.properties.code_deleg ? "q" + (d.properties.code_deleg % 10) + "-9" : "unidentified";
+        .text(function(d) {return d.properties.name_2;});
+    
+    features.exit().remove();     
+}
+function updateLabels(svg, json, path) {
+    if (_addLabels){
+	var labels =  svg.selectAll("text")
+            .data(json.features, function (d) {
+		return d.properties.name_1 + " " + d.properties.name_2;
+            });
+	
+	labels
+            .enter()
+            .append("svg:text")
+            .attr("class","delegation-label")
+            .text(function(d){return d.properties.name_2;})  
+            .attr("x", function(d){return (path.centroid(d))[0];}) 
+            .attr("y", function(d){return (path.centroid(d))[1];}) 
+            .attr("fill", "black") 
+            .attr("text-anchor", "middle") ;
+    
+	labels.exit().remove(); 
+    } else {
+	svg.selectAll("text").remove();
     }
+}
 
-   function click(d, i) {
-       console.log("click", d, i);
-   }
+function updateInfoBox() {
+    d3.select("#info-title")
+	.text(_data.features[0].properties.name_1);
+    var table = d3.select("#info-table").select("tbody");
+    table.selectAll("tr").remove();
 
-   function mouseover(d, i) {
-       var oldColor = d3.select(this).style("fill");
-       var newColor = d3.hsl(oldColor).darker().toString();
-       d3.select(this)
-           .transition()
-           .duration(100)
-           .ease("bounce")
-           .style("fill",newColor)
-           .style("stroke-width", "4px")
-   }
+    if (false && selectedDelegation){
+	table.selectAll("tr")
+	    .data(_selectedFeature.properties)
+            .append("tr")
+            .text(function(d){return d });
+    } else {
+	var counter = 1;
+        var tr = table.selectAll("tr")
+	    .data(_data.features, function (d) {
+		return d.properties.name_1 + " " + d.properties.name_2;
+            })
+	    .enter()
+	    .append("tr")
+	var td = tr.selectAll("td")
+	    .data(function(d) { return [d.properties.code_deleg, d.properties.name_deleg, d.properties.name_2]; })
+	    .enter().append("td")
+	    .text(function(d) {return d; });
+    }
+}
 
-   function mouseout(d, i) {
-       d3.select(this)
-           .transition()
-           .duration(100)
-           .ease("bounce")
-           .style("fill",null)
-           .style("stroke-width",null)
-           .style("stroke", null)
-   }
-   function project(width) {
+function quantize(d) {
+    return d.properties.code_deleg ? "q" + (d.properties.code_deleg % 10) + "-9" : "unidentified";
+}
 
-   }
-})
-};
+
+function click(d, i) {
+/*
+    var previousDelegation = _selectedDelegation;
+    var previousFeature = _selectedFeature;
+    _selectedDelegation = (_selectedDelegation != null && _selectedDelegation == d.properties.name_2) ? null : d.properties.name_2;
+    
+    if (previousDelegation && previousFeature){
+	d3.select(previousFeature)
+            .transition()
+            .duration(100)
+            .ease("bounce")
+            .style("fill",null)
+            .style("stroke-width", null)
+    }
+    if (_selectedDelegation) {
+	_selectedFeature = this;
+	d3.select(_selectedFeature)
+            .transition()
+            .duration(100)
+            .ease("bounce")
+            .style("fill","#FF0000")
+            .style("stroke-width", "4px")
+    }
+    updateInfoBox()
+*/
+}
+
+function mouseover(d, i) {
+    if (_selectedDelegation == null) {
+	var oldColor = d3.select(this).style("fill");
+	var newColor = d3.hsl(oldColor).darker().toString();
+	d3.select(this)
+            .transition()
+            .duration(100)
+            .ease("bounce")
+            .style("fill",newColor)
+            .style("stroke-width", "4px")
+    }
+}
+
+function mouseout(d, i) {
+    if (_selectedDelegation == null) {
+	d3.select(this)
+            .transition()
+            .duration(100)
+            .ease("bounce")
+            .style("fill",null)
+            .style("stroke-width", null)
+    }
+}
